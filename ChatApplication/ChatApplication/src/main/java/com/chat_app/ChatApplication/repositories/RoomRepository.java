9593283a32ad18c71edb3f382f3d8ce941c4d77f
Priyanshu <pv254424@gmail.com>
2026-04-01 23:06:54 +0530

    making a chat app

---
+++ ChatApplication/ChatApplication/src/main/java/com/chat_app/ChatApplication/repositories/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.chat_app.ChatApplication.repositories;
+
+import com.chat_app.ChatApplication.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+    Room findRoomById(String roomId);
+}
