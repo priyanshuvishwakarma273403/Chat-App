887d0de051a716d76e329fd9f65dadd916696e9d
Priyanshu <pv254424@gmail.com>
2026-04-02 00:01:16 +0530

    all fixes

--- chat-frontend/src/App.jsx
+++ chat-frontend/src/App.jsx
@@ -1,11 +1,18 @@
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import toast from "react-hot-toast";
+import JoinCreateChat from "./components/JoinCreateChat";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className='bg-red-500'>
-      <h1>Chat Application</h1>
+    <div>
+      <JoinCreateChat />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- chat-frontend/src/main.jsx
+++ chat-frontend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import { Toaster } from 'react-hot-toast'
+import { ChatProvider } from './context/ChatContext.jsx'
+import AppRoutes from './config/Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <Toaster position="top-center" />
+    <ChatProvider>
+      <AppRoutes />
+    </ChatProvider>
+  </BrowserRouter>
 )
