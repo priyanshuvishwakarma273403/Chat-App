d5a82c1381a87b23e77082681a03cf3aa65ae108
Priyanshu <pv254424@gmail.com>
2026-04-01 23:36:06 +0530

    room contoller complete

--- ChatApplication/ChatApplication/src/main/java/com/chat_app/ChatApplication/controllers/RoomController.java
+++ ChatApplication/ChatApplication/src/main/java/com/chat_app/ChatApplication/controllers/RoomController.java
@@ -1,11 +1,14 @@
 package com.chat_app.ChatApplication.controllers;
 
+import com.chat_app.ChatApplication.entities.Message;
 import com.chat_app.ChatApplication.entities.Room;
 import com.chat_app.ChatApplication.repositories.RoomRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rooms")
@@ -31,7 +34,34 @@ public class RoomController {
     }
 
 
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> joinRoom(
+            @PathVariable String roomId
+    ){
+        Room room = roomRepository.findByRoomId(roomId);
+        if(room == null){
+            return ResponseEntity.badRequest().body("Room not found !!");
+        }
+        return ResponseEntity.ok(room);
+    }
 
+    @GetMapping("/{roomId}/messages")
+    public ResponseEntity<List<Message>> getMessages(
+            @PathVariable String roomId,
+            @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(value = "size" , defaultValue = "20", required = false) int size
+    ){
+        Room room = roomRepository.findByRoomId(roomId);
+        if(room == null){
+            return ResponseEntity.badRequest().build();
+        }
 
+        List<Message> messages = room.getMessages();
+        int start = Math.max(0, messages.size() - (page + 1) * size);
+        int end = Math.min(messages.size(), start + size);
+        List<Message> paginatedMessage = messages.subList(start, end);
+        return ResponseEntity.ok(paginatedMessage);
+
+    }
 
 }
